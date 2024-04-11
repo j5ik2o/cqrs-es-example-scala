956173d241673d3a26b4f6871293725e7f0ac1ea
Junichi Kato <j5ik2o@gmail.com>
2024-04-11 16:19:24 +0900

    fix: refactor

--- modules/command/domain/src/main/scala/com/github/j5ik2o/cqrs/es/example/scala/command/domain/groupChat/MemberRole.scala
+++
@@ -1,6 +0,0 @@
-package com.github.j5ik2o.cqrs.es.example.scala.command.domain.groupChat
-
-enum MemberRole {
-  case Admin
-  case Member
-}
--- modules/command/domain/src/main/scala/com/github/j5ik2o/cqrs/es/example/scala/command/domain/groupChat/MessageBody.scala
+++
@@ -1,5 +0,0 @@
-package com.github.j5ik2o.cqrs.es.example.scala.command.domain.groupChat
-
-final case class MessageBody(value: String) {
-  require(value.nonEmpty)
-}
